6266f65fdd386ee5645537f5d4bf9529c02b0ac7
qubiX00 <115372907+qubiX00@users.noreply.github.com>
2022-10-09 13:44:18 +0200

    Add files via upload
    Update

--- src/index.js
+++ src/index.js
@@ -1,37 +1,34 @@
-import { w2popup } from 'https://rawgit.com/vitmalina/w2ui/master/dist/w2ui.es6.min.js'
+import { w2popup,w2alert,w2confirm,w2prompt, w2utils } from 'https://rawgit.com/vitmalina/w2ui/master/dist/w2ui.es6.min.js'
 
 window.popup2 = function() {
     w2popup.open({
         title: 'Plasma Editor',
-        text: '<textarea style="width: 600px; height: 188px; resize: none;" autocomplete="on"></textarea>',
-        actions: ['Ok', 'Cancel'],
-        width: 500,
-        height: 300,
-        modal: true,
-        showClose: true,
-        showMax: true,
-        onMax(evt) {
-            console.log('max', evt)
-        },
-        onMin(evt) {
-            console.log('min', evt)
-        },
-        onKeydown(evt) {
-            console.log('keydown', evt)
-        }
+        width: 800,
+        height: 500,
+        text: '<textarea style="width: 780px; height: 370px; resize: none;" autocomplete="on"></textarea>',
+        actions: ['Show Message'],
+        showMax: true
     })
-        .then((evt) => {
-            console.log('popup ready')
-        })
-        .close(evt => {
-            console.log('popup clsoed')
-        })
-        .ok((evt) => {
-            console.log('ok', evt)
-            w2popup.close()
-        })
-        .cancel((evt) => {
-            console.log('cancel', evt)
-            w2popup.close()
-        })
+	    .showMessage(() => {
+        showMessage('Plasma Editor Beta 2 by Badless and qubiX00')
+    })
 }
+
+window.popup3 = function() {
+    w2popup.open({
+        title: 'Welcome',
+        text: 'Welcome to NeuOS: Open Source OS runnable in your browser!'
+    })
+}
+
+window.showMessage = function(text) {
+    w2popup.message({
+        text,
+        width: 400,
+        height: 180,
+        hideOn: ['esc', 'click']
+    });
+}
+
+
+
