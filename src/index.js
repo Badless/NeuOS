93551f79b81376721376e66dec19568fbdb453ad
DevBadless <79766539+Badless@users.noreply.github.com>
2022-10-09 15:26:30 +0200

    Toolbar, favicon

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import { w2popup,w2alert,w2confirm,w2prompt, w2utils } from 'https://rawgit.com/vitmalina/w2ui/master/dist/w2ui.es6.min.js'
+import { w2popup,w2alert,w2confirm,w2prompt, w2utils, w2toolbar } from 'https://rawgit.com/vitmalina/w2ui/master/dist/w2ui.es6.min.js'
 
 window.popup2 = function() {
     w2popup.open({
         title: 'Plasma Editor',
         width: 800,
         height: 500,
-        text: '<textarea style="width: 780px; height: 370px; resize: none;" autocomplete="on"></textarea>',
+        text: '<textarea id="plasma-editor" style="width: 780px; height: 370px; resize: none;" autocomplete="on"></textarea>',
         actions: ['Show Message'],
-        showMax: true
+        //showMax: true
     })
 	    .showMessage(() => {
         showMessage('Plasma Editor Beta 2 by Badless and qubiX00')
@@ -30,5 +30,25 @@ window.showMessage = function(text) {
     });
 }
 
-
-
+new w2toolbar({
+    box: '#toolbar',
+    name: 'toolbar',
+    items: [
+        { type: 'button', id: 'item1', text: 'Welcome', icon: 'w2ui-icon-info' },
+        { type: 'break' },
+        { type: 'button', id: 'item2', text: 'Editor', icon: 'w2ui-icon-pencil' },
+        { type: 'break' },
+        { type: 'drop',  id: 'item3', text: 'Power', icon: 'w2ui-icon-settings',
+            html: '<button onclick="self.close()">Power Off</button>'
+        },
+        { type: 'break' },
+    ],
+    onClick(event) {
+        if (event.target == 'item1') {
+            popup3();
+        }
+        if (event.target == 'item2') {
+            popup2();
+        }
+    }
+})
